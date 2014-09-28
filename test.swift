cfe5c06a6ce9c8805dd2f2a9d36749420e13801c
Behrang Noruzi Niya <behrangn@gmail.com>
2014-09-28 11:11:35 +0330

    Add tokenize function

--- test.swift
+++ test.swift
@@ -1,35 +1,13 @@
 import Yaml
 
-typealias Spec = [String: [String: ()->()]]
+assert(Yaml.load("") == .Null, "empty input should be null")
+assert(Yaml.load("null") == .Null, "null should be null")
+assert(Yaml.load("Null") == .Null, "Null should be null")
+assert(Yaml.load("NULL") == .Null, "NULL should be null")
+assert(Yaml.load("~") == .Null, "~ should be null")
 
-let spec: Spec = [
-  "YamlValue.load": [
+assert(Yaml.load("NuLL") != .Null, "NuLL should NOT be null")
 
-    "should return YamlNull for ``": {
-      assert(Yaml.load("") == Yaml.YamlNull)
-    },
+assert(Yaml.load("# comment line") == .Null, "A comment input should be null")
 
-    "should return YamlNull for `null` or `Null` or `NULL` or `~`": {
-      assert(Yaml.load("null") == Yaml.YamlNull)
-      assert(Yaml.load("Null") == Yaml.YamlNull)
-      assert(Yaml.load("NULL") == Yaml.YamlNull)
-      assert(Yaml.load("~") == Yaml.YamlNull)
-    },
-
-    "should NOT return YamlNull for `NuLL`": {
-      assert(Yaml.load("NuLL") == nil)
-    }
-  ]
-]
-
-let indent = "    "
-let checkmark = UnicodeScalar(0x2713)
-
-for (key, value) in spec {
-  println(key)
-  for (key, value) in value {
-    print("\(indent)   \(key)")
-    value()
-    println("\r\(indent) \(checkmark) \(key)")
-  }
-}
+println("Done.")
--- yaml.swift
+++ yaml.swift
@@ -1,14 +1,84 @@
 import Foundation
 
+enum TokenType: String, Printable {
+  case Comment = "comment"
+  case Null = "null"
+
+  var description: String {
+    return self.toRaw()
+  }
+}
+
+typealias TokenPattern = (type: TokenType, pattern: String)
+typealias TokenMatch = (type: TokenType, match: String)
+
+let tokenPatterns: [TokenPattern] = [
+  (.Comment, "^#[^\\n]*"),
+  (.Null, "^(null|Null|NULL|~|$)")
+]
+
+func context (var text: String) -> String {
+  let endIndex = advance(text.startIndex, 25, text.endIndex)
+  text = text.substringToIndex(endIndex)
+  text = text.stringByReplacingOccurrencesOfString("\n", withString: "\\n")
+  text = text.stringByReplacingOccurrencesOfString("\"", withString: "\\\"")
+  return "near \"\(text)\""
+}
+
+func tokenize (var text: String) -> (error: String?, tokens: [TokenMatch]?) {
+  var matches: [TokenMatch] = []
+  next:
+  while countElements(text) > 0 {
+    for tokenPattern in tokenPatterns {
+      if let range = text.rangeOfString(tokenPattern.pattern, options: .RegularExpressionSearch) {
+        matches.append(TokenMatch(tokenPattern.type, text.substringWithRange(range)))
+        text = text.substringFromIndex(range.endIndex)
+        continue next
+      }
+    }
+    return (context(text), nil)
+  }
+  return (nil, matches)
+}
+
 public enum Yaml {
 
-  case YamlNull
+  case Null
+  case Invalid(String)
 
-  public static func load (text: String) -> Yaml? {
-    if let range = text.rangeOfString("^(null|Null|NULL|~|$)", options: .RegularExpressionSearch) {
-      return YamlNull
-    } else {
-      return nil
+  public static func load (text: String) -> Yaml {
+    let result = tokenize(text)
+    if let error = result.error {
+      return .Invalid(error)
     }
+    return .Null
   }
+}
+
+public func == (lhs: Yaml, rhs: Yaml) -> Bool {
+  switch lhs {
+
+  case .Null:
+    switch rhs {
+    case .Null:
+      return true
+    default:
+      return false
+    }
+
+  case .Invalid(let ls):
+    switch rhs {
+    case .Invalid(let rs):
+      return ls == rs
+    default:
+      return false
+    }
+
+  default:
+    return false
+  }
+}
+
+public func != (lhs: Yaml, rhs: Yaml) -> Bool {
+  return !(lhs == rhs)
 }
